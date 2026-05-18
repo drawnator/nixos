28dfe9fa478cab71256895fd861100ef05cf0d27
guilherme <thedrawnator@gmail.com>
2026-05-18 11:18:27 -0300

    Fallback to core.editor
    if no message is passed as parameter or to the input
    
    this also enables adding comments to the commit

--- sync.sh
+++ sync.sh
@@ -3,7 +3,11 @@ git add .
 if [ $# -eq 0 ]; then
     read -p "Please enter the commit message: " INPUT
 fi
-MESSAGE=${1:-$INPUT}
-git commit -m "$MESSAGE"
+if [ -z "$INPUT"]; then
+    git commit
+else
+    MESSAGE=${1:-$INPUT}
+    git commit -m "$MESSAGE"
+fi 
 git push
 sudo nixos-rebuild switch
