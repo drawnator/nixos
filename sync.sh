a34c9d7edda972747e005306e3a35667e5458ce9
guilherme <thedrawnator@gmail.com>
2026-05-18 10:39:34 -0300

    crude sync script

---
+++ sync.sh
@@ -0,0 +1,10 @@
+git pull
+git add .
+if [ $# -eq 0 ]; then
+    read -p "Please enter the commit message: " INPUT
+fi
+MESSAGE=${1:-$INPUT}
+echo $1
+echo git commit -m "$MESSAGE"
+git push
+nixos-rebuild switch
