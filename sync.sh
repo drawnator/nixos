daf721708901ab171cd42e1e841f85ed2771389b
guilherme <thedrawnator@gmail.com>
2026-05-18 11:16:29 -0300

    first fully remote sync

--- sync.sh
+++ sync.sh
@@ -4,7 +4,6 @@ if [ $# -eq 0 ]; then
     read -p "Please enter the commit message: " INPUT
 fi
 MESSAGE=${1:-$INPUT}
-echo $1
 git commit -m "$MESSAGE"
 git push
-nixos-rebuild switch
+sudo nixos-rebuild switch
