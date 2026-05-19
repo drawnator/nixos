92541709fa2247878077905e337d143429f9da27
guilherme <thedrawnator@gmail.com>
2026-05-19 18:21:41 -0300

    fixed networkmanager-openvpn

--- system/network.nix
+++ system/network.nix
@@ -10,11 +10,15 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = [
+      pkgs.networkmanager-openvpn
+    ];
+  };
   networking.useDHCP = false;
   environment.systemPackages = with pkgs; [
     openvpn
-    networkmanager-openvpn
   ];
  };
 }
