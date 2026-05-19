93188888eaf9020e632663a68fb2e6b3d69ca8c0
guilherme <thedrawnator@gmail.com>
2026-05-19 10:14:55 -0300

    add group to user

--- user/g.nix
+++ user/g.nix
@@ -7,9 +7,11 @@
  ];
  config = {
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.groups.g = {}; 
   users.users.g = {
     isNormalUser = true;
      description = "guilherme";
+     group = "g";
      extraGroups = [ "networkmanager" "wheel" ];
      packages = with pkgs; [
      #thunderbird
