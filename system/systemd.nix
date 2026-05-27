1cabeef7fba90fa552e3f27ece89604a2b2269cf
guilherme <thedrawnator@gmail.com>
2026-05-27 19:39:55 -0300

    fix tmpfiles

--- system/systemd.nix
+++ system/systemd.nix
@@ -2,8 +2,9 @@
 { lib, config, pkgs, ... }:
 {
  config = {
+    #https://man7.org/linux/man-pages/man5/tmpfiles.d.5.html
     systemd.tmpfiles.rules = [
-      "e /home/g/Downloads 0755 g g - 7d"
+      "e /home/g/Downloads - - - 7d"
     ];
  };
 }
